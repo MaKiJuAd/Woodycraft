719010151a466be2b334a115664763270b970d7e
Byfralt <kycorvaisier@gmail.com>
2023-10-05 09:33:04 +0200

    Test Commit

---
+++ database/migrations/2023_10_02_082717_create_produits_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::create('produits', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('image');
+            $table->foreignId('cat_id')->constrained()->onUpdate('restrict')->onDelete('restrict');
+            $table->float('price');
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produits');
+    }
+};
